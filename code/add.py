2082469bd9b479a68672b1832413b28c7b31142f
hbandar <vardha18harsha@gmail.com>
2023-11-08 19:23:37 -0500

    add checks to prevent future date selection

--- code/add.py
+++ code/add.py
@@ -2,7 +2,7 @@ import helper
 import logging
 from telebot import types
 from telegram_bot_calendar import DetailedTelegramCalendar, LSTEP
-from datetime import datetime
+from datetime import datetime,date
 
 option = {}
 
@@ -24,22 +24,26 @@ def run(message, bot):
     date = datetime.today()
     @bot.callback_query_handler(func=DetailedTelegramCalendar.func())
     def cal(c):
+        chat_id = c.message.chat.id
         result, key, step = DetailedTelegramCalendar().process(c.data)
 
         if not result and key:
             bot.edit_message_text(
                 f"Select {LSTEP[step]}",
-                c.message.chat.id,
+                chat_id,
                 c.message.message_id,
                 reply_markup=key,
             )
         elif result:
-            date = result
-            category_selection(message,bot,date)
+            data = datetime.today().date()
+            if (result > data):
+                bot.send_message(chat_id,"Cannot select future dates, Please try /add command again with correct dates")
+            else:
+                category_selection(message,bot,result)
 
 def category_selection(msg,bot,date):
     try:
-        print(date)
+        # print(date)
         markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
         markup.row_width = 2
         for c in helper.getSpendCategories():
@@ -102,56 +106,6 @@ def post_category_selection(message, bot, date):
         bot.send_message(chat_id, "Please select a menu option from below:")
         bot.send_message(chat_id, display_text)
 
-def record_expense(message, bot, previous_expenses):
-    print("In function to record expense")
-    selection = message.text
-    print(selection)
-    markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
-    markup.row_width = 2
-    if selection == "Y" or selection == "y":
-        for record in previous_expenses:
-            markup.add(record)
-        msg = bot.reply_to(message, "Select the expense you want to repeat", reply_markup=markup)
-        bot.register_next_step_handler(msg, post_expense_selection, bot)
-    else:
-        for c in helper.getSpendCategories():
-            markup.add(c)
-        markup.add("Add new category")
-        msg = bot.reply_to(message, "Select Category", reply_markup=markup)
-        bot.register_next_step_handler(msg, post_category_selection, bot)
-
-def post_expense_selection(message,bot):
-    chat_id = message.chat.id
-    expense_record = message.text
-    expense_data = expense_record.split(",")
-    amount = expense_data[2]
-    category = expense_data[1]
-    print(amount)
-    amount_value = helper.validate_entered_amount(amount)  # validate
-    try:
-        if amount_value == 0:  # cannot be $0 spending
-            raise Exception("Spent amount has to be a non-zero number.")
-        date_of_entry = datetime.today().strftime(helper.getDateFormat())
-        date_str, category_str, amount_str = (
-            str(date_of_entry),
-            str(category),
-            str(amount_value),
-        )
-        helper.write_json(
-            add_user_record(
-                chat_id, "{},{},{}".format(date_str, category_str, amount_str)
-            )
-        )
-        bot.send_message(
-            chat_id,
-            "The following expenditure has been recorded: You have spent ${} for {} on {}".format(
-                amount_str, category_str, date_str
-            ),
-        )
-        helper.display_remaining_budget(message, bot, category)
-    except Exception as e:
-        logging.exception(str(e))
-        bot.reply_to(message, "Oh no. " + str(e))
 
 def post_amount_input(message, bot, selected_category, date):
     """
@@ -201,7 +155,7 @@ def add_user_record(chat_id, record_to_be_added):
     user_list = helper.read_json()
     if str(chat_id) not in user_list:
         user_list[str(chat_id)] = helper.createNewUserRecord()
-        
+
     user_list[str(chat_id)]["data"].append(record_to_be_added)
     print(user_list)
     return user_list
