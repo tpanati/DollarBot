66ccd5d6364991c700fdf5132e3f7e1201260792
tpanati <tpanati@ncsu.edu>
2023-11-15 14:45:46 -0500

    chore: Update the existing framework for delete.py

--- test/test_delete.py
+++ test/test_delete.py
@@ -114,7 +114,7 @@ def test_deleteHistory():
     result = delete.deleteHistory("sample_chat_id")
     
     # Assert that the user_list entry is removed
-    assert "sample_chat_id" not in user_list
+    assert "sample_chat_id" not in result
      # Assert that the data is cleared in the result
     expected_result = {"sample_chat_id": {"data": [], "budget": {"overall": "0", "category": {}}}}
     assert result == expected_result
