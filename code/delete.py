7f2f8ba0eff95a36836560affd9987f868bb0ebb
Tejaswini <tpanati@ncsu.edu>
2023-11-16 23:10:08 -0500

    chore: Edit pylint errors

--- code/delete.py
+++ code/delete.py
@@ -14,28 +14,28 @@ def run(message, bot):
     data saved in myDollarBot i.e their chat ID has been logged before, run calls the deleteHistory(chat_id):
     to remove it. Then it ensures this removal is saved in the datastore.
     """
+    global user_list
     dateFormat = helper.getDateFormat()
     chat_id = message.chat.id
     delete_history_text = ""
-
+    user_list = helper.read_json()
     try:
-        user_list = helper.read_json()  # Read the user_list from the file
         if str(chat_id) in user_list and helper.getUserHistory(chat_id) is not None:
             curr_day = datetime.now()
             prompt = "Enter the corresponding date in the given format or Enter All to delete the entire history\n"
             prompt += f"\n\tExample day: {curr_day.strftime(dateFormat)}\n"
             reply_message = bot.reply_to(message, prompt)
-            bot.register_next_step_handler(reply_message, process_delete_argument, bot, user_list, chat_id)
+            bot.register_next_step_handler(reply_message, process_delete_argument, bot)
         else:
             delete_history_text = "No records there to be deleted. Start adding your expenses to keep track of your spendings!"
             bot.send_message(chat_id, delete_history_text)
-
+    
     except Exception as ex:
         print("Exception occurred : ")
         logging.error(str(ex), exc_info=True)
         bot.reply_to(message, "Processing Failed - \nError : " + str(ex))
 
-def process_delete_argument(message, bot, user_list, chat_id):
+def process_delete_argument(message, bot):
     """
     This function receives the choice that user inputs for delete and asks for a confirmation. 'handle_confirmation'
     is called next.
@@ -45,9 +45,11 @@ def process_delete_argument(message, bot, user_list, chat_id):
     :return: None
     """
     text = message.text
+    chat_id = message.chat.id
+
     date = text
     if text.lower() == "all":
-        helper.write_json(deleteHistory(chat_id, user_list))
+        helper.write_json(deleteHistory(chat_id))
         bot.send_message(chat_id, "History has been deleted!")
     else:
         if date is None:
@@ -69,9 +71,9 @@ def process_delete_argument(message, bot, user_list, chat_id):
             markup.add("No")
             response_str += "\nReply Yes or No"
             response = bot.reply_to(message, response_str, reply_markup=markup)
-            bot.register_next_step_handler(response, handle_confirmation, bot, records_to_delete, user_list, chat_id)
+            bot.register_next_step_handler(response, handle_confirmation, bot, records_to_delete)
 
-def handle_confirmation(message, bot, records_to_delete, user_list, chat_id):
+def handle_confirmation(message, bot, records_to_delete):
     """
     Deletes the transactions in the previously chosen time period if the user chooses 'yes'.
 
@@ -80,30 +82,30 @@ def handle_confirmation(message, bot, records_to_delete, user_list, chat_id):
     :type: object
     :return: None
     """
-    chat_id_str = str(chat_id)
 
+    chat_id = str(message.chat.id)
     if message.text.lower() == "yes":
-        if chat_id_str in user_list:
+        if str(chat_id) in user_list:
             # Get the user's data
-            user_data = user_list.get(chat_id_str, {}).get('data', [])
+            user_data = user_list.get(str(chat_id), {}).get('data', [])
             # Remove the specified records
             user_data = [record for record in user_data if record not in records_to_delete]
             # Update the userlist with the modified data
-            user_list[chat_id_str]['data'] = user_data
+            user_list[str(chat_id)]['data'] = user_data
         helper.write_json(user_list)
         bot.send_message(message.chat.id, "Successfully deleted records")
     else:
         bot.send_message(message.chat.id, "No records deleted")
 
 # function to delete a record
-def deleteHistory(chat_id, user_list):
+def deleteHistory(chat_id):
     """
     deleteHistory(chat_id): It takes 1 argument for processing - chat_id which is the
     chat_id of the user whose data is to deleted from the user list. It removes this entry from the user list.
     """
-    chat_id_str = str(chat_id)
-    if chat_id_str in user_list:
-        user_list[chat_id_str]["data"] = []
-        user_list[chat_id_str]["budget"]["overall"] = str(0)
-        user_list[chat_id_str]["budget"]["category"] = {}
+    global user_list
+    if str(chat_id) in user_list:
+        user_list[str(chat_id)]["data"] = []
+        user_list[str(chat_id)]["budget"]["overall"] = str(0)
+        user_list[str(chat_id)]["budget"]["category"] = {}
     return user_list
