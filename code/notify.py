612f2ddbaa49717dfa625585840b3c0a0d0e6912
hbandar <vardha18harsha@gmail.com>
2023-11-16 22:47:19 -0500

    write docstrings for monthly and notifier commands

--- code/notify.py
+++ code/notify.py
@@ -4,6 +4,19 @@ from notifier import TelegramNotifier
 configs = Properties()
 
 def notify(chat_id, cat, amount):
+    """
+    Sends a notification to a Telegram chat about a budget exceeding.
+
+    Parameters:
+    - chat_id (str): The chat ID where the notification will be sent.
+    - cat (str): The category for which the budget exceeded.
+    - amount (str): The amount by which the budget exceeded.
+
+    This function reads the Telegram API token from a configuration file,
+    creates a TelegramNotifier object, and sends a notification to the specified
+    chat ID about the budget exceeding for the given category and amount.
+    """
+
     print("inside notify")
     with open("user.properties", "rb") as read_prop:
         configs.load(read_prop)
